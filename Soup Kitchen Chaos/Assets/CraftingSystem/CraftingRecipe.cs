4191834cefdd6f0424c8136b9446d8924fc85ef3
martinWff <48971889+martinWff@users.noreply.github.com>
2024-04-06 00:00:08 +0100

    Crafting System

---
+++ Soup Kitchen Chaos/Assets/CraftingSystem/CraftingRecipe.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Recipe")]
+public class CraftingRecipe : ScriptableObject
+{
+    public List<Ingredient> ingredients;
+
+    public float heatingDuration = -1;
+    public bool requiresHeating => heatingDuration > -1;
+
+    public Ingredient output;
+}
+
+[System.Serializable]
+public struct Ingredient
+{
+    public string name;
+    public int units;
+}
+
+[System.Serializable]
+public class Food
+{
+    public Ingredient ingredient;
+
+    public float workingProgress;
+
+    public bool isCompleted => workingProgress <= 0;
+
+    public bool isBurned;
+
+    public bool canBeUsed => isCompleted && !isBurned;
+
+    public Food(Ingredient ingr)
+    {
+        this.ingredient = ingr;
+    }
+}
