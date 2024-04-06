35afe785f275f80948646cc7cabef3308f6db73a
Francisco Santos <49843015+fransantos1@users.noreply.github.com>
2024-04-06 10:46:57 +0100

    fixed movement

--- Soup Kitchen Chaos/Assets/Scripts/Player.cs
+++ Soup Kitchen Chaos/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    [SerializeField]
+    private float rotationSpeed;
 
     void Start()
     {
@@ -14,11 +16,19 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb.velocity = movement * moveSpeed;
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity.normalized);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+        transform.Translate(movementDirection * moveSpeed * inputMagnitude * Time.deltaTime, Space.World);
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+
     }
 }
