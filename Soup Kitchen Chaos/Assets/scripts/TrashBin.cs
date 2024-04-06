000253ad52cdd4de516aade973a416c1cd86e1b9
martinWff <48971889+martinWff@users.noreply.github.com>
2024-04-06 11:18:25 +0100

    Trashbin Added

---
+++ Soup Kitchen Chaos/Assets/scripts/TrashBin.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour, IStorable<Food>
+{
+    //by doing nothing the item is being destroyed
+    public bool Store(GameObject instigator,Food item)
+    {
+        return true;
+    }
+}
