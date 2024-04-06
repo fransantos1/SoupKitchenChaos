52a2cbd333732f66ecd63f39e3eb17f0b32b3be7
Francisco Santos <49843015+fransantos1@users.noreply.github.com>
2024-04-06 11:30:50 +0100

    started crafting,:3

--- Soup Kitchen Chaos/Assets/scripts/CraftingSystem/CraftingUnit.cs
+++ Soup Kitchen Chaos/Assets/scripts/CraftingSystem/CraftingUnit.cs
@@ -62,7 +62,7 @@ public class CraftingUnit : MonoBehaviour, IInteractable, IStorable<Food>
 
     public virtual void Interact(GameObject instigator)
     {
-
+        Craft();
     }
 
     public virtual bool Store(GameObject instigator,Food item)
